5ca63b9dabce5f1b5f68cd7839490cfbd5604a04
SofiaBartoli <sofiadbartoli@gmail.com>
2026-01-07 17:15:56 -0300

    Se crea entidad y dto para reservas

---
+++ src/modules/reservations/dtos/reservation.dto.ts
@@ -0,0 +1,16 @@
+import { IsDateString, Matches } from 'class-validator';
+
+export class CreateReservationDto {
+  @IsDateString()
+  activityDate: Date;
+
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/, {
+    message: 'startTime must be HH:mm or HH:mm:ss',
+  })
+  startTime: string;
+
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/, {
+    message: 'endTime must be HH:mm or HH:mm:ss',
+  })
+  endTime: string;
+}
---
+++ src/modules/reservations/entities/reservations.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/users.entity';
+import { ReservationStatus } from 'src/common/enum/reservations.enum';
+
+@Entity('reservations')
+export class Reservation {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'date' })
+  activityDate: Date;
+
+  @Column({ type: 'time' })
+  startTime: string;
+
+  @Column({ type: 'time' })
+  endTime: string;
+
+  @Column({
+    type: 'enum',
+    enum: ReservationStatus,
+    default: ReservationStatus.pending,
+  })
+  status: ReservationStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User, (user) => user.reservations, { eager: true })
+  user: User;
+}
