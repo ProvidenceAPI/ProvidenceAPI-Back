2a247adb123a036b298b9935a2b7337d316e7ef4
Luz Canon <luzadca@hotmail.com>
2026-01-08 13:18:35 -0500

    configuracion tabla postgres y Railway

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,16 +1,23 @@
 import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 
-dotenvConfig();
+dotenvConfig({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env.production'
+      : '.env.development',
+});
 
 export default registerAs('typeorm', () => {
+  // Si existe DATABASE_URL (Railway), úsala
   if (process.env.DATABASE_URL) {
     return {
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, //process.env.NODE_ENV !== 'production',
-      logging: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+      dropSchema: false,
+      logging: process.env.NODE_ENV === 'development',
       ssl:
         process.env.NODE_ENV === 'production'
           ? {
@@ -29,6 +36,7 @@ export default registerAs('typeorm', () => {
     password: process.env.DB_PASSWORD,
     autoLoadEntities: true,
     synchronize: process.env.NODE_ENV !== 'production',
+    dropSchema: false, // 👈 false para NO borrar datos
     logging: process.env.NODE_ENV === 'development',
   };
 });
