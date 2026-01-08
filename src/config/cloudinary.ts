0dd0bcaa638acb48e72bb5abe4c0fb32bfcbba10
SofiaBartoli <sofiadbartoli@gmail.com>
2026-01-08 16:47:19 -0300

    Agregado de return necesario

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -8,13 +8,15 @@ dotenvconfig({
       : '.env.development',
 });
 
-export const CloudinaryConfig = {
-  provide: 'Cloudinary',
+export const CloudinaryProvider = {
+  provide: 'CLOUDINARY',
   useFactory: () => {
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
+
+    return cloudinary;
   },
 };
