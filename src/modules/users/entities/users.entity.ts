f8f6ccd22fef8f3c22b0dec8bed6b824ad7c685e
SofiaBartoli <sofiadbartoli@gmail.com>
2026-01-13 16:06:30 -0300

    Cambio en package.json

--- src/modules/users/entities/users.entity.ts
+++ src/modules/users/entities/users.entity.ts
@@ -16,7 +16,6 @@ export class User {
   })
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
   @ApiProperty({ example: 'Valentina' })
   @Column({ type: 'varchar', length: 50, nullable: false })
   name: string;
