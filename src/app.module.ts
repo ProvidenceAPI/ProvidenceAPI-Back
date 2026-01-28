a297c30d885fdddf1b0eccc664d6717aaad3a082
SofiaBartoli <sofiadbartoli@gmail.com>
2026-01-27 21:18:07 -0300

    Importacion de testimonials

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { SubscriptionsModule } from './modules/subscriptions/subscriptions.modul
 import { MercadoPagoModule } from './modules/mercadopago/mercadopago.module';
 import { AdminNotificationsModule } from './modules/admin-notifications/admin-notifications.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { TestimonialsModule } from './modules/testimonials/testimonials.module';
 
 @Module({
   imports: [
@@ -47,6 +48,7 @@ import { NotificationsModule } from './modules/notifications/notifications.modul
     SubscriptionsModule,
     AdminNotificationsModule,
     NotificationsModule,
+    TestimonialsModule,
   ],
   controllers: [],
   providers: [],
